02a853b7bfc79fcbdbad7d5d1ade1a3b35000de5
shirishsharma <shirish.sharma@paytail.com>
2023-10-09 00:26:01 +0530

    added hilt dagger.

---
+++ app/src/main/java/com/example/authentationapp/RetrofitModule.kt
@@ -0,0 +1,28 @@
+package com.example.authentationapp
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl("https://api.first.org/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+}
